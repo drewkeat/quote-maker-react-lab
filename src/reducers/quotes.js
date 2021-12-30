4ac382a76c9307c3c42b1159f502b28b6c73164e
Drew Keat <thedrewkeat@gmail.com>
2021-12-29 20:01:55 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,20 @@
 export default (state = [], action) => {
-  return state;
+  switch(action.type){
+    case "ADD_QUOTE":
+      let quote = {...action.quote, votes: 0}
+      return state.concat(quote);
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId) 
+    case "UPVOTE_QUOTE":
+      return state.map(quote => quote.id === action.quoteId ? {...quote, votes: quote.votes + 1} : quote)
+    case "DOWNVOTE_QUOTE":
+      quote = state.filter(quote => quote.id === action.quoteId)[0]
+      if (quote.votes > 0) {
+        return state.map(quote => quote.id === action.quoteId ? {...quote, votes: quote.votes - 1} : quote)
+      } else {
+        return state
+      }
+    default:
+      return state;
+  }
 }
